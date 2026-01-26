c6b2e9d6356ef2a571318e23f16125c8ceaf9ce8
gargyogesh1 <106339179+gargyogesh1@users.noreply.github.com>
2026-01-26 16:40:11 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import "./src/cron/dailyBlog.cron.js";
 // ✅ Start server
 import app from "./src/app.js";
 
-app.listen(5000, () => {
-  console.log("✅ Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
 });
